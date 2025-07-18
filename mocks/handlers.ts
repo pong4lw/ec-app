68cb36eb59b1c87e6e05cdf5274e49f6d1da9fe9
hiroki-honda <hiroki.hon@gmail.com>
2025-07-18 10:24:03 +0900

    Deployエラー対策

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,29 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // mocks/handlers.ts
-import { http } from 'msw';
+import { http, HttpResponse } from 'msw';
 
 export const handlers = [
-  http.get('/api/products', (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        { id: 'p1', name: 'テスト商品', price: 2000, imageUrl: '', description: 'テスト説明' },
-        { id: 'p2', name: 'サンプルTシャツ', price: 2900, imageUrl: '', description: 'サンプル説明' },
-      ])
-    );
+  // 商品一覧取得API
+  http.get('/api/products', async () => {
+    return HttpResponse.json([
+      { id: 'p1', name: 'テスト商品', price: 2000, imageUrl: '', description: 'テスト説明' },
+      { id: 'p2', name: 'サンプルTシャツ', price: 2900, imageUrl: '', description: 'サンプル説明' },
+    ]);
   }),
 
-  http.get('/api/products/:id', (req, res, ctx) => {
-    const { id } = req.params;
-    return res(
-      ctx.status(200),
-      ctx.json({
-        id,
-        name: 'テスト商品詳細',
-        price: 2000,
-        imageUrl: '',
-        description: 'テスト説明詳細',
-      })
-    );
+  // 商品詳細取得API
+  http.get('/api/products/:id', async ({ params }) => {
+    const { id } = params;
+    return HttpResponse.json({
+      id,
+      name: 'テスト商品詳細',
+      price: 2000,
+      imageUrl: '',
+      description: 'テスト説明詳細',
+    });
   }),
 ];
